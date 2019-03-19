dc0e40115802150d0d82ee63ee58b9a7da9a055e
sulan <su1900@163.com>
2019-03-20 02:19:05 +0800

    代理作业

---
+++ src/main/java/com/sul/gphomework/App.java
@@ -0,0 +1,13 @@
+package com.sul.gphomework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
